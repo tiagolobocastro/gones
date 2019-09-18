4f766ec7467cbc24ea114a60765d39bd87a15409
Tiago Castro <tiagolobocastro@gmail.com>
2019-09-19 00:55:34 +0100

    moves main package to the top, this seems to make goland happy

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
-import "github.com/tiagolobocastro/gones"
+import "github.com/tiagolobocastro/gones/nes"
 
 func main() {
 	nes := gones.NewNES(true, "C:\\Users\\Tiago\\workspace\\nes-test-roms\\cpu_dummy_reads\\source\\hello_nes\\hello.nes")
